1d36a1dae6f794a326291b721e0dac1db4ed28ae
Matias Garcia <matiasigarcia91@gmail.com>
2021-04-15 11:54:41 +0200

    set up page skeletons and navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 // src/App.js
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Link to='/' style={{ marginRight: 15 }}>
+        Home
+      </Link>
+      <Link to='/login' style={{ marginRight: 15 }}>
+        Login
+      </Link>
+      <Link to='/signup'>Signup</Link>
+    </div>
+  );
+};
 
 export default function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
         {/* more pages to be added here later */}
+        <Route path='/signup' component={Signup} />
+        <Route path='/login' component={Login} />
         <Route path='/' component={Homepage} />
       </Switch>
     </div>
